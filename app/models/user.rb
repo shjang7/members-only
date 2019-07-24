1cbf4ea2f0d6bc864233dad253dfb1859adc269c
wathigo <wathigosimon@gmail.com>
2019-07-24 12:03:09 -0400

    Add a remember_token to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   before_save   :downcase_email
+  before_create :generate_token
   validates :name, presence: true, length: { minimum: 4,
                                              maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -9,7 +10,15 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
-  def downcase_email
-    email.downcase!
+  def generate_token
+    token = SecureRandom.urlsafe_base64
+    self.remember_token = Digest::SHA1.hexdigest(token.to_s)
   end
+
+  private
+
+    # Converts email to all lower-case.
+    def downcase_email
+      email.downcase
+    end
 end
